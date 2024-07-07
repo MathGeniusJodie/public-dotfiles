fe61389b60abc667f9bd127169124de592751529
root <root@localhost.localdomain>
2024-07-07 04:01:37 -0400

    added login.sh

--- startx.sh
+++ startx.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-termux-x11 :1 -xstartup "dbus-launch --exit-with-session xfce4-session"
+termux-x11 :1 -xstartup "GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.0 dbus-launch --exit-with-session xfce4-session"
