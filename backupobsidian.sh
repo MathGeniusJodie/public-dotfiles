0394bd10219f4ec547f78cd285b59e72d69e45e9
MathGeniusJodie <mail@jodie.website>
2025-03-13 18:11:06 -0400

    use tar for backups

--- backupobsidian.sh
+++ backupobsidian.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 archive_path="obsidian_vault_$(date +%Y%m%d_%H%M%S).zip"
-zip -r "$archive_path" "RemoteVault"
+#zip -r "$archive_path" "RemoteVault"
+tar -cf "$archive_path" "RemoteVault"
 rclone copy "$archive_path" "Dropbox:obsidian_backup"
 rm "$archive_path"
