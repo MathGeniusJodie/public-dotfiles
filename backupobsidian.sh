398ef78cd5c904d24570c9a80e8013556c8c37e5
MathGeniusJodie <mail@jodie.website>
2025-03-19 15:05:42 -0400

    added git to obsidian backups

--- backupobsidian.sh
+++ backupobsidian.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 archive_path="obsidian_vault_$(date +%Y%m%d_%H%M%S).zip"
+cd RemoteVault || exit 1
+git add --all && git commit -m "backup $(date +%Y%m%d_%H%M%S)"
+cd ..
 #zip -r "$archive_path" "RemoteVault"
 tar -cf "$archive_path" "RemoteVault"
 rclone copy "$archive_path" "Dropbox:obsidian_backup"
